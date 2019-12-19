d7a2774018747ae2e3e1dcac43475f9cbb0aaabd
Осипов Николай Алексеевич <osipov.n@ccam.cmc.unn.net>
2019-12-19 14:43:23 +0300

    Add Etap 1.0

--- LAB3-POSTFIX/LAB3-POSTFIX/lexeme.cpp
+++ LAB3-POSTFIX/LAB3-POSTFIX/lexeme.cpp
@@ -9,9 +9,19 @@ enum lexeme_type
 	number,
 	plus,
 	minus,
+	un_minus,
 	mult,
 	divide,
-	power
+	power,
+	left_br,
+	right_br
+};
+
+enum status
+{
+	start,
+	waitOperator,
+	waitNumber
 };
 
 struct lexeme
@@ -24,9 +34,69 @@ struct lexeme
 	{
 		type = number;
 		val = res;
+		pr = -1;
 	}
 	lexeme(lexeme_type type, double val):type(type), val(val){}
+	lexeme(lexeme_type type, int pr):type(type), pr(pr), val(-1.0) {}
 };
+
+lexeme getNextLexeme(string s, int pos, int &nexPos)
+{
+	nexPos = pos;
+	char tmp = s[pos];
+	lexeme l;
+	if (tmp >= '0' || tmp <= '9')
+	{
+		l = lexeme(number, tmp - '0');
+		nexPos++;
+	}
+	else if (tmp == '+')
+	{
+		l = lexeme(lexeme_type::plus, 1);
+	}
+	return l;
+}
+
+vector<lexeme> Parse(string s)
+{
+	lexeme tmp;
+	vector<lexeme> res;
+	status st = start;
+	int pos = 0;
+	while (pos != s.size())
+	{
+		tmp = getNextLexeme(s, pos, pos);
+		if (st == start)
+		{
+			if (tmp.type == number)
+			{
+				res.push_back(tmp);
+				st = waitOperator;
+			}
+			else if (tmp.type == left_br)
+			{
+				res.push_back(tmp);
+				st = waitNumber;
+			}
+			else
+			{
+				throw "error";
+			}
+		}
+		else if (st == waitOperator)
+		{
+			if (tmp.type == lexeme_type::plus)
+			{
+				res.push_back(tmp);
+			}
+		}
+		else if (st == waitNumber)
+		{
+
+		}
+	}
+	return res;
+}
 
 double PolishCalculate(vector<lexeme> data)
 {
@@ -91,7 +161,10 @@ double PolishCalculate(vector<lexeme> data)
 int main()
 {
 	vector<lexeme> data = {lexeme(number, 2), lexeme(number, 3), lexeme(lexeme_type::power, -1), lexeme(number, 5), lexeme(lexeme_type::mult, -1) };
-	cout << PolishCalculate(data);
+	cout << PolishCalculate(data) << "\n";
+	auto vectorLexeme = Parse("5+");
+	for (auto l : vectorLexeme)
+		cout << l.val << " ";
 	system("pause");
 	return 0;
 }
